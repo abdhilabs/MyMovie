7f4154cf8a3163da3797b39228b03e0d2aaceaad
Abdhilabs <rizaabdhi@gmail.com>
2020-05-14 00:48:55 +0700

    add navigation component

---
+++ app/src/main/java/com/abdhilabs/mymovie/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.abdhilabs.mymovie
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+    }
+}
